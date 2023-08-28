628f7375a15e159a47d0fd6874393d7c598e5a7a
Evmos <longdieu12x@gmail.com>
2023-08-29 00:16:33 +0700

    add build

---
+++ build/static/js/33.5f2e83ca.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[33],{1070:function(e,t,r){"use strict";r.r(t);r(1);var c=r(19);t.default=function(){return Object(c.jsx)("div",{})}}}]);
+//# sourceMappingURL=33.5f2e83ca.chunk.js.map
